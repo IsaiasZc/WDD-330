c4c7882aa3a91644ac9d60ad5b463907403e0e83
IsaiasZc <78005126+IsaiasZc@users.noreply.github.com>
2022-10-22 14:31:35 -0500

    week 6 page

--- main.js
+++ main.js
@@ -19,6 +19,10 @@ const links = [
     label: 'Week-5 notes',
     url: './week5/'
   },
+  {
+    label: 'Week-6 notes',
+    url: './week6/'
+  }
 ];
 
 const LIST = document.getElementById('list');
